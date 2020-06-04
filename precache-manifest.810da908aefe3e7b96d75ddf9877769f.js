cd28626cfe7b3558564c6188e19d042a71ed2620
dep-vol <alex.tecto.msk@gmail.com>
2020-06-04 19:26:45 +0300

    Updates

---
+++ precache-manifest.810da908aefe3e7b96d75ddf9877769f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c977f233a1f90e436d90f7e46ca9ac0a",
+    "url": "/tasks_admin/index.html"
+  },
+  {
+    "revision": "568c837b356afc5440d3",
+    "url": "/tasks_admin/static/css/main.48956b67.chunk.css"
+  },
+  {
+    "revision": "e67cbefdb5cf67ed3485",
+    "url": "/tasks_admin/static/js/2.418fe997.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tasks_admin/static/js/2.418fe997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568c837b356afc5440d3",
+    "url": "/tasks_admin/static/js/main.1e7e8dfb.chunk.js"
+  },
+  {
+    "revision": "c9dce0bcafe129eb745e",
+    "url": "/tasks_admin/static/js/runtime-main.99941666.js"
+  }
+]);
